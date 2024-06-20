687f5ff71a077240197375d7cbf53e59786cfd33
amac81 <arnaldocanelas@gmail.com>
2024-06-20 09:42:32 +0100

    project created

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+/**
+* Programa desafio Java vendas 1
+* Devsuperior.club
+
+* @author  Arnaldo Canelas
+* @version 1.0
+* @since   2024-06-20
+*/
+
+package application;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	}
+}
