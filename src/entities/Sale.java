e54d0e410f61ff35be0a6a47c3f27836af2f2b7e
amac81 <arnaldocanelas@gmail.com>
2024-06-20 09:52:40 +0100

    class Sale create

---
+++ src/entities/Sale.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class Sale {
+	private Integer month;
+	private Integer year;
+	private String seller;
+	private Integer items;
+	private Double total;
+	
+	public Sale() {}
+
+	public Sale(Integer month, Integer year, String seller, Integer items, Double total) {
+		this.month = month;
+		this.year = year;
+		this.seller = seller;
+		this.items = items;
+		this.total = total;
+	}
+
+	public Integer getMonth() {
+		return month;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getSeller() {
+		return seller;
+	}
+
+	public Integer getItems() {
+		return items;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+	
+	
+	
+	
+
+}
